e036e1382e693ea971a0f907dbec358aab8a055a
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-05-05 08:18:07 -0400

    add some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dual-list-picker';
+
+  public sourceList:string[] = [
+    "Wheel",
+    "Door",
+    "Trunk",
+    "Engine"
+  ];
+
+  public destinationList:string[] = [
+    "Hood"
+  ];
+
+  public drop($event: CdkDragDrop<string[]>) {
+    if($event.previousContainer === $event.container) {
+      moveItemInArray(this.sourceList,$event.previousIndex,$event.currentIndex);
+    }else {
+      transferArrayItem(
+        $event.previousContainer.data,
+        $event.container.data,
+        $event.previousIndex,
+        $event.currentIndex
+      );
+    }
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
